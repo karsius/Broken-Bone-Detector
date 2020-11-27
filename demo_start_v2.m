6068ea2b79375b341f22538a3ac31a032c778480
Computer one <computerone@Computers-MacBook-Air.local>
2020-11-26 22:01:12 -0500

    working with output

--- demo_start_v2.m
+++ demo_start_v2.m
@@ -6,17 +6,19 @@ addpath(genpath('code/'));
 addpath('Imgs/');
 addpath('edges-master/');
 
+imName = 'arm4.png';
 
 
-img = imread(['Imgs', filesep, 'arm3.png']);
 
-img = imrotate(img,33,'crop');
+img = imread(['Imgs', filesep, imName]);
+
+img = imrotate(img,30,'crop');
 
 % this process was under imadjust(img), and is moved up here
 % this should find and blur the edge of the actual xray img and the black background
 % with an average filter 20*20
 img = removeEdge(img,20);
-figure('Name', 'edge_removed');imagesc(img);axis image;axis off; colormap gray;
+%figure('Name', 'edge_removed');imagesc(img);axis image;axis off; colormap gray;
 
 % crops the image by 30%
 img = img(ceil(size(img,1)*0.3):size(img,1),ceil(size(img,2)*0.3):size(img,2));
@@ -24,7 +26,7 @@ img = img(ceil(size(img,1)*0.3):size(img,1),ceil(size(img,2)*0.3):size(img,2));
 % increases the images contrast 
 img = imadjust(img); 
 
-figure('Name', 'edge_removed');imagesc(img);axis image;axis off; colormap gray;
+%figure('Name', 'edge_removed');imagesc(img);axis image;axis off; colormap gray;
 
 t = nanmedian(img,'all');
 m = max(img,[],'all');
@@ -50,25 +52,18 @@ hold all
 %Order lines by probability
 lines = sortrows(lines, -5);
 ttlLines = size(lines,1);
-for i = 1:90
-    %plot the top 90 lines
+for i = 1:ttlLines
+    %plot all lines
     line([lines(i,1) lines(i,3)], [lines(i,2) lines(i,4)],'Color', rand(1,3), 'LineWidth', 3);
 end
 
-%please use code in Evaluation code.zip to evaluate the performance of the line segmentation algorithm
+
+sl = sortLines(lines);
+disp(sl);
+bb = isBroken(sl);
+output(imName, fig, bb);
 
 
-[lines2] = mcmlsd2Algo(lines,img);
-fig = figure;
-imshow(img);
-hold all
-%Order lines by probability
-lines = sortrows(lines2, -5);
-ttlLines = size(lines2,1);
-for i = 1:90
-    %plot the top 90 lines
-    line([lines2(i,1) lines2(i,3)], [lines2(i,2) lines2(i,4)],'Color', rand(1,3), 'LineWidth', 3);
-end
 
 function imout = adcontrst(im)
     im = im2double(im);
@@ -85,12 +80,11 @@ function imout = adcontrst(im)
     imout = (im./msecResize) .* ratio;
     imout = im2uint8(imout);
     imagesc(imout);axis image;colormap gray;
-    disp('work');
 end
 
 
-
 function imb = black2NaN(im,t)
+    %changes all pixels values to 0 that are below threshold value t
     for i = 1:size(im,1)
         for j = 1:size(im,2)
             if im(i,j) < t
@@ -119,4 +113,48 @@ function imb = removeEdge(im, rad)
         end
     end
     imb = canvas;
+end
+
+function l = sortLines(lines)
+    l = hypot(lines(:,3)-lines(:,1), lines(:,4)-lines(:,2));
+    l = sort(l, 'descend');
+end
+
+function br = isBroken(lines)
+    % check to see if there are more than 1 line detected
+    if length(lines) > 1
+       % check if the longest line has a pair of similar length
+       if lines(2) >= lines(1)*0.80
+           br = 0;
+           disp("not broken");
+       else
+           br = 1;
+           disp("broken");
+       end
+    else
+         disp("Error: only 1 line detected");  
+    end
+end
+
+function output(imName, fig, broken)
+
+    % check if output folder exists 
+    if ~exist('output','dir')
+        mkdir('output');
+        mkdir('output/broken');
+        mkdir('output/not_broken');
+    end
+    
+    if broken
+        fname = strcat("output/broken/", imName);
+        saveas(fig,fname);
+        out = {imName, 1};
+        writecell(out,"output/log.csv",'WriteMode', 'append');
+    else
+        fname = strcat("output/not_broken/", imName);
+        saveas(fig,fname);
+        out = {imName, 0};
+        writecell(out,"output/log.csv",'WriteMode', 'append');
+    end
+
 end
